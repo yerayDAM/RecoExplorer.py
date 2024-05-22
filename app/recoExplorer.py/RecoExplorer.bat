9e4e8b1e4909afbe409b3ea94f92aa96eebeeedc
yerayDAM <167007108+yerayDAM@users.noreply.github.com>
2024-05-22 00:30:54 +0000

    Update RecoExplorer.bat

--- app/recoExplorer.py/RecoExplorer.bat
+++ app/recoExplorer.py/RecoExplorer.bat
@@ -1,20 +1,8 @@
 @echo off
 
-REM Comprobando si Python o Python3 ya están instalados
-python --version >nul 2>&1
-set PYTHON_INSTALLED=%errorlevel%
-python3 --version >nul 2>&1
-set PYTHON3_INSTALLED=%errorlevel%
+REM Ejecutar el script de Python en segundo plano sin abrir una nueva ventana
+start /B python3 main.py
 
-REM Cambiar al directorio desde donde se ejecutó el script
-cd /d "%~dp0"
-
-REM Ejecutar el script de Python desde el directorio actual
-if %PYTHON_INSTALLED% neq 0 ( 
-    python3 recoExplorer.py\mainGetApi.py
-) else (
-    python recoExplorer.py\mainGetApi.py
-)
 
 
 REM Obtener el ID del proceso de Python
