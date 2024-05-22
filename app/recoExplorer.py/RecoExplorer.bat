2744f1039506c38e5d48be399935cea5a6bb81b2
yerayDAM <167007108+yerayDAM@users.noreply.github.com>
2024-05-22 00:09:01 +0000

    Update RecoExplorer.bat

--- app/recoExplorer.py/RecoExplorer.bat
+++ app/recoExplorer.py/RecoExplorer.bat
@@ -1,12 +1,24 @@
 @echo off
 
+REM Comprobar si python o python3 está disponible
+where python >nul 2>&1
+if %errorlevel% equ 0 (
+    set PYTHON_CMD=python
+) else (
+    where python3 >nul 2>&1
+    if %errorlevel% equ 0 (
+        set PYTHON_CMD=python3
+    ) else (
+        echo No se encontró python ni python3 en el PATH
+        exit /b 1
+    )
+)
+
 REM Ejecutar el script de Python en segundo plano sin abrir una nueva ventana
-start /B python3 main.py
-
-
+start /B %PYTHON_CMD% main.py
 
 REM Obtener el ID del proceso de Python
-for /f "tokens=2" %%I in ('tasklist /fi "imagename eq python3.exe" /fo table /nh') do (
+for /f "tokens=2" %%I in ('tasklist /fi "imagename eq %PYTHON_CMD%.exe" /fo table /nh') do (
     set "PID=%%I"
 )
 
